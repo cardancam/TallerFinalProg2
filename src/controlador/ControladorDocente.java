75dc0969f0bbbfee0d5833b4cfd2f2a0f9e8fcb5
CarlosCamacho222 <cdcamacho30@gmail.com>
2025-05-28 00:26:15 -0500

    Actualizacion en clase Docente y ControladorDocente.

--- src/controlador/ControladorDocente.java
+++ src/controlador/ControladorDocente.java
@@ -1,6 +1,15 @@
 package controlador;
+import modelo.Docente;
+import vista.VistaDocente;
 
 public class ControladorDocente {
+    private Docente modelo;
+    private VistaDocente vista;
 
+    public ControladorDocente(Docente modelo, VistaDocente vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+    
     
 }
